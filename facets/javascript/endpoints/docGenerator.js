34f681bf493f7b19452d62ba1778a27817bed3af
tonysviews <tonysviews@gmail.com>
2022-07-05 02:35:15 +0000

    Create JS script for endpointdocGenerator

--- facets/javascript/endpoints/docGenerator.js
+++ facets/javascript/endpoints/docGenerator.js
@@ -1,39 +1,53 @@
-const docGenerator = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/docGenerator/${parameters.moduleCode}`, baseUrl);
-	return fetch(url.toString(), {
-		method: 'POST', 
-		headers : new Headers({
- 			'Content-Type': 'application/json'
-		}),
-		body: JSON.stringify({
-			
-		})
-	});
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "docGeneratorRequest",
+  "id" : "docGeneratorRequest",
+  "default" : "Schema definition for docGenerator",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const docGeneratorForm = (container) => {
-	const html = `<form id='docGenerator-form'>
-		<div id='docGenerator-moduleCode-form-field'>
-			<label for='moduleCode'>moduleCode</label>
-			<input type='text' id='docGenerator-moduleCode-param' name='moduleCode'/>
-		</div>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-	const moduleCode = container.querySelector('#docGenerator-moduleCode-param');
-
-	container.querySelector('#docGenerator-form button').onclick = () => {
-		const params = {
-			moduleCode : moduleCode.value !== "" ? moduleCode.value : undefined
-		};
-
-		docGenerator(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "docGeneratorResponse",
+  "id" : "docGeneratorResponse",
+  "default" : "Schema definition for docGenerator",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
 }
 
-export { docGenerator, docGeneratorForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class docGenerator extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("docGenerator", "POST");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new docGenerator();
